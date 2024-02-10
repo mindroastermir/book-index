303cc18f05c94cb6d0601e48ee7c5898aeecebb0
Saleem Meer <smeer@pcon.as>
2024-02-10 14:30:11 +0100

    added data.js

---
+++ js/data.js
@@ -0,0 +1,9 @@
+const books = [
+{
+    id: 1,
+    bookNo: '2213',
+    name: 'آداب تلاوت مع طریقہ حفظ قرآن وحفظ قراآت',
+    author: 'قاری رحیم بخش پانی پتی',
+    link: 'https://kitabosunnat.com/kutub-library/adab-e-talawat-ma-tareka-hifaz-e-quran-w-hifze-qirat'
+}
+];
