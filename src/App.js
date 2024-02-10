edf92604638b20475579650e4baa47354b908cb0
Saleem Meer <smeer@pcon.as>
2024-02-11 00:00:10 +0100

    added a component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Box(){
+  return(
+      <h1>Box Component</h1>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <h1>React Book Index</h1>
       <h2>This is a test page</h2>
+      <Box/>
     </div>
   );
 }
